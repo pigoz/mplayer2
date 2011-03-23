19be6f17e7abf4db7140f97e63e295d319c69613
Kovensky <diogomfranco@gmail.com>
2011-03-23 02:36:17 +0000

    Use Windows API functions for the console OSD
    Make it actually work as intended instead of printing garbage before
    every line.
    
    Patch from http://oss.netfarm.it/mplayer/patches/xx_term_osd_win32.diff

--- mplayer.c
+++ mplayer.c
@@ -1658,6 +1658,25 @@ void set_osd_subtitle(struct MPContext *mpctx, subtitle *subs)
     }
 }
 
+#ifdef _WIN32
+#include <io.h>
+static void term_osd_eraseline(void)
+{
+    DWORD wr;
+    COORD pos;
+    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO cinfo;
+    GetConsoleScreenBufferInfo(hOut, &cinfo);
+    pos.X = 0;
+    pos.Y = cinfo.dwCursorPosition.Y - 1;
+    FillConsoleOutputCharacter(hOut, ' ', cinfo.dwSize.X, pos, &wr);
+    FillConsoleOutputAttribute(hOut, cinfo.wAttributes, cinfo.dwSize.X, pos, &wr);
+    SetConsoleCursorPosition(hOut, pos);
+}
+#else
+#define term_osd_eraseline() printf("%s", opts->term_osd_esc)
+#endif
+
 /**
  * \brief Update the OSD message line.
  *
@@ -1688,9 +1707,10 @@ static void update_osd_msg(struct MPContext *mpctx)
             osd_set_text(osd, msg->msg);
             if (mpctx->sh_video)
                 vo_osd_changed(OSDTYPE_OSD);
-            else if (opts->term_osd)
-                mp_msg(MSGT_CPLAYER, MSGL_STATUS, "%s%s\n", opts->term_osd_esc,
-                       msg->msg);
+            else if (opts->term_osd) {
+                term_osd_eraseline();
+                mp_msg(MSGT_CPLAYER, MSGL_STATUS, "%s\n", msg->msg);
+            }
         }
         return;
     }
@@ -1762,7 +1782,7 @@ static void update_osd_msg(struct MPContext *mpctx)
     // Clear the term osd line
     if (opts->term_osd && osd->osd_text[0]) {
         osd->osd_text[0] = 0;
-        printf("%s\n", opts->term_osd_esc);
+        term_osd_eraseline();
     }
 }
 
