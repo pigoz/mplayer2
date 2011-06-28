pigoz/mplayer2

commit 11d3d67971793532ec17a1cadf93504b44e0a594

Rename guiInterface_t member NoWindow MovieWindow.
This variable holds the current state of visibility of the movie window
and is easier to understand if it is phrased in the positive.


git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@33743 b3059339-0415-0410-9bf9-f77b7e298cf2

gui/interface.c
@@ -596,7 +596,7 @@ int guiGetEvent(int type, void *arg)
         guiInfo.AudioOnly = (int)arg;
 
         if (guiInfo.AudioOnly) {
-            guiInfo.NoWindow = True;
+            guiInfo.MovieWindow = False;
             wsVisibleWindow(&guiApp.subWindow, wsHideWindow);
         } else
             wsVisibleWindow(&guiApp.subWindow, wsShowWindow);
@@ -723,7 +723,7 @@ int guiGetEvent(int type, void *arg)
             guiInfo.FPS = sh->fps;
         }
 
-        if (guiInfo.NoWindow)
+        if (!guiInfo.MovieWindow)
             wsVisibleWindow(&guiApp.subWindow, wsHideWindow);
 
         if (guiInfo.StreamType == STREAMTYPE_STREAM)
@@ -858,12 +858,12 @@ int guiGetEvent(int type, void *arg)
         {
             int i = 0;
 
-            guiInfo.NoWindow = False;
+            guiInfo.MovieWindow = True;
 
             while (video_out_drivers[i++]) {
                 if (video_out_drivers[i - 1]->control(VOCTRL_GUISUPPORT, NULL) == VO_TRUE) {
                     if ((video_driver_list && !gstrcmp(video_driver_list[0], (char *)video_out_drivers[i - 1]->info->short_name)) && (video_out_drivers[i - 1]->control(VOCTRL_GUI_NOWINDOW, NULL) == VO_TRUE)) {
-                        guiInfo.NoWindow = True;
+                        guiInfo.MovieWindow = False;
                         break;
                     }
                 }
@@ -1013,7 +1013,7 @@ int guiGetEvent(int type, void *arg)
 
 void guiEventHandling(void)
 {
-    if (!guiInfo.Playing || guiInfo.NoWindow)
+    if (!guiInfo.Playing || !guiInfo.MovieWindow)
         wsHandleEvents();
 
     gtkEventHandling();

gui/interface.h
@@ -172,7 +172,7 @@ typedef struct {
 
     int MovieWidth;
     int MovieHeight;
-    int NoWindow;
+    int MovieWindow;
 
     float Volume;
     float Balance;

gui/ui/actions.c
@@ -40,7 +40,7 @@ int uiGotoTheNext = 1;
 
 void uiFullScreen(void)
 {
-    if (guiInfo.NoWindow && guiInfo.Playing)
+    if (!guiInfo.MovieWindow && guiInfo.Playing)
         return;
 
     if (guiInfo.Playing && guiApp.subWindow.isFullScreen) {
@@ -114,7 +114,7 @@ void uiEnd(void)
         guiInfo.TimeSec       = 0;
         guiInfo.Position      = 0;
         guiInfo.AudioChannels = 0;
-        guiInfo.NoWindow      = False;
+        guiInfo.MovieWindow   = True;
 
 #ifdef CONFIG_DVDREAD
         guiInfo.DVD.current_title   = 1;

gui/ui/gtk/menu.c
@@ -615,7 +615,7 @@ GtkWidget * create_PopUpMenu( void )
   AddMenuItem( window1, (const char*)prefs_xpm, Menu,MSGTR_MENU_Preferences, evPreferences );
   AddMenuItem( window1, (const char*)equalizer_xpm, Menu,MSGTR_Equalizer, evEqualizer );
 
-  if ( guiInfo.NoWindow == False )
+  if ( guiInfo.MovieWindow )
    {
     int b1 = 0, b2 = 0, b_half = 0;
     AddSeparator( Menu );

gui/ui/main.c
@@ -229,7 +229,7 @@ NoPause:
    case evStop:
 	guiInfo.Playing=GUI_STOP;
 	uiState();
-	guiInfo.NoWindow=False;
+	guiInfo.MovieWindow=True;
 	break;
 
    case evLoadPlay:

commit 03e243da5d4242147391a5b28015f166706d24b3

Fix bug introduced in r33743.
The GUI could no longer start in fullscreen video mode.


git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@33751 b3059339-0415-0410-9bf9-f77b7e298cf2

gui/interface.c
@@ -52,6 +52,7 @@
 
 guiInterface_t guiInfo = {
     .StreamType = STREAMTYPE_DUMMY,
+    .MovieWindow = True,
     .Balance    = 50.0f
 };
 