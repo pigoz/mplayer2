76de893b78225cb34a2ea34b56ed13c3417f18bd
wm4 <wm4@mplayer2.org>
2011-11-19 22:29:24 +0100

    vo_gl3: fork from vo_gl
    This is probably a mistake.

--- libvo/video_out.c
+++ libvo/video_out.c
@@ -84,6 +84,7 @@ extern struct vo_driver video_out_vdpau;
 extern struct vo_driver video_out_xv;
 extern struct vo_driver video_out_gl_nosw;
 extern struct vo_driver video_out_gl;
+extern struct vo_driver video_out_gl3;
 extern struct vo_driver video_out_dga;
 extern struct vo_driver video_out_sdl;
 extern struct vo_driver video_out_3dfx;
@@ -179,6 +180,7 @@ const struct vo_driver *video_out_drivers[] =
 #endif
 #ifdef CONFIG_GL
         &video_out_gl,
+        &video_out_gl3,
 #endif
 #ifdef CONFIG_DGA
         &video_out_dga,
