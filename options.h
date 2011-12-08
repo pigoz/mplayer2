3f6b1f01bcc54645d4ae7e70dbebbf7c5b98cc65
Stefano Pigozzi <stefano.pigozzi@gmail.com>
2011-12-08 11:17:59 +0100

    core: cocoa: X11: add option -cursor-autohide-delay
    Add option -cursor-autohide-delay to control the number of milliseconds
    with no user interaction before the mouse cursor is hidden.
    
    Add two negative values with special useful meanings:
    * A value of -1 prevents the cursor from hiding (useful for users
    with multiple displays).
    * A value of -2 prevents the cursor from showing upon activity.
    
    The default is 1 second to keep the behaviour consistent with the
    past X11 backend implementation.

--- options.h
+++ options.h
@@ -21,6 +21,7 @@ typedef struct MPOpts {
     int fullscreen;
     int vo_dbpp;
     float vo_panscanrange;
+    int cursor_autohide_delay;
     int requested_colorspace;
     int requested_input_range;
     int requested_output_range;
