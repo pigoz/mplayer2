36f6ae84c7fc395f10ee4171ab43df6b4ca95fed
Stefano Pigozzi <stefano.pigozzi@gmail.com>
2011-12-11 12:26:00 +0100

    vo_corevideo: use cocoa_common to display the window
    Change to code to use cocoa_common to create and manage the window. This
    doesn't impact external GUIs since they all read from the shared_buffer.

--- libvo/cocoa_common.m
+++ libvo/cocoa_common.m
@@ -55,6 +55,7 @@ struct vo_cocoa_state {
     NSAutoreleasePool *pool;
     GLMPlayerWindow *window;
     NSOpenGLContext *glContext;
+    NSOpenGLPixelFormat *pixelFormat;
 
     NSSize current_video_size;
     NSSize previous_video_size;
@@ -213,8 +214,8 @@ int vo_cocoa_create_window(struct vo *vo, uint32_t d_width,
             (NSOpenGLPixelFormatAttribute)0
         };
 
-        NSOpenGLPixelFormat *pixelFormat = [[NSOpenGLPixelFormat alloc] initWithAttributes:attrs];
-        s->glContext = [[NSOpenGLContext alloc] initWithFormat:pixelFormat shareContext:nil];
+        s->pixelFormat = [[[NSOpenGLPixelFormat alloc] initWithAttributes:attrs] autorelease];
+        s->glContext = [[NSOpenGLContext alloc] initWithFormat:s->pixelFormat shareContext:nil];
 
         create_menu();
 
@@ -344,6 +345,16 @@ int vo_cocoa_swap_interval(int enabled)
 {
     [s->glContext setValues:&enabled forParameter:NSOpenGLCPSwapInterval];
     return 0;
+}
+
+void *vo_cocoa_cgl_context(void)
+{
+    return [s->glContext CGLContextObj];
+}
+
+void *vo_cocoa_cgl_pixel_format(void)
+{
+    return [s->pixelFormat CGLPixelFormatObj];
 }
 
 void create_menu()
