bff7ec8dc1e9c22f13062cb91e797633b876f0d4
Stefano Pigozzi <stefano.pigozzi@gmail.com>
2012-04-24 15:27:19 +0200

    add iosurface file

---
+++ libvo/iosurface.h
@@ -0,0 +1,29 @@
+/*
+ * IOSurface helper functions
+ *
+ * This file is part of mplayer2.
+ *
+ * mplayer2 is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * mplayer2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with mplayer2.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * IOSurface is a API for MacOS >= 10.6 that allows to share kernel managed
+ * image data across processes. This is particularly useful in mplayer2 to
+ * implement external GUIs.
+ */
+
+#ifndef MPLAYER_IOSURFACE_H
+#define MPLAYER_IOSURFACE_H
+
+#endif /* MPLAYER_IOSURFACE_H */
