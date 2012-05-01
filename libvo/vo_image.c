a91ae75afb708d6b60ba26d69a53dd6900a8ddaa
wm4 <wm4@mplayer2.org>
2012-05-01 12:27:38 +0200

    vo_image: add outdir option
    The function checked_mkdir is copy & pasted from vo_jpeg.
    xxx this function sucks, it should obviously be rewritten

--- libvo/vo_image.c
+++ libvo/vo_image.c
@@ -20,10 +20,13 @@
 #include <string.h>
 #include <math.h>
 #include <stdbool.h>
+#include <sys/stat.h>
 
 #include <libswscale/swscale.h>
 
 #include "config.h"
+#include "osdep/io.h"
+#include "path.h"
 #include "talloc.h"
 #include "mp_msg.h"
 #include "libvo/video_out.h"
@@ -37,6 +40,7 @@
 
 struct priv {
     struct image_writer_opts *opts;
+    char *outdir;
 
     int frame;
 
@@ -46,6 +50,47 @@ struct priv {
     struct mp_csp_details colorspace;
 };
 
+static void checked_mkdir(const char *buf, int verbose) {
+    struct stat stat_p;
+
+    // hurr
+    struct { char *short_name; } info = { "vo_image" };
+
+    if ( mkdir(buf, 0755) < 0 ) {
+        switch (errno) { /* use switch in case other errors need to be caught
+                            and handled in the future */
+            case EEXIST:
+                if ( mp_stat(buf, &stat_p ) < 0 ) {
+                    mp_msg(MSGT_VO, MSGL_ERR, "%s: %s: %s\n", info.short_name,
+                           _("This error has occurred"), strerror(errno) );
+                    mp_msg(MSGT_VO, MSGL_ERR, "%s: %s %s\n", info.short_name,
+                           _("Unable to access"), buf);
+                }
+                if ( !S_ISDIR(stat_p.st_mode) ) {
+                    mp_msg(MSGT_VO, MSGL_ERR, "%s: %s %s\n", info.short_name,
+                           buf, _("already exists, but is not a directory."));
+                }
+                if ( !(stat_p.st_mode & S_IWUSR) ) {
+                    mp_msg(MSGT_VO, MSGL_ERR, "%s: %s - %s\n", info.short_name,
+                           buf, _("Output directory already exists, but is not writable."));
+                }
+
+                mp_msg(MSGT_VO, MSGL_INFO, "%s: %s - %s\n", info.short_name,
+                       buf, _("Output directory already exists and is writable."));
+                break;
+
+            default:
+                mp_msg(MSGT_VO, MSGL_ERR, "%s: %s: %s\n", info.short_name,
+                       _("This error has occurred"), strerror(errno) );
+                mp_msg(MSGT_VO, MSGL_ERR, "%s: %s - %s\n", info.short_name,
+                       buf, _("Unable to create output directory."));
+        } /* end switch */
+    } else if ( verbose ) {
+        mp_msg(MSGT_VO, MSGL_INFO, "%s: %s - %s\n", info.short_name,
+               buf, _("Output directory successfully created."));
+    } /* end if */
+}
+
 static int config(struct vo *vo, uint32_t width, uint32_t height,
                   uint32_t d_width, uint32_t d_height, uint32_t flags,
                   uint32_t format)
@@ -54,6 +99,9 @@ static int config(struct vo *vo, uint32_t width, uint32_t height,
 
     p->d_width = d_width;
     p->d_height = d_height;
+
+    if (p->outdir && vo->config_count < 1)
+        checked_mkdir(p->outdir, 1);
 
     return 0;
 }
@@ -81,8 +129,13 @@ static uint32_t draw_image(struct vo *vo, mp_image_t *mpi)
     char filename[80];
     snprintf(filename, sizeof(filename), "%08d.%s", p->frame,
              image_writer_file_ext(p->opts));
-    printf("Save %s!\n", filename);
-    write_image(&tmp, &p->colorspace, p->opts, filename);
+    char *path = filename;
+    if (p->outdir)
+        path = mp_path_join(NULL, bstr(p->outdir), bstr(filename));
+    printf("Save '%s'!\n", path);
+    write_image(&tmp, &p->colorspace, p->opts, path);
+    if (path != filename)
+        talloc_free(path);
 
     (p->frame)++;
 
@@ -150,6 +203,8 @@ const struct vo_driver video_out_image =
         .opts = (struct m_option[]) {
             {"-", (void *) &image_writer_conf, &m_option_type_subconfig_struct, M_OPT_MERGE, 0, 0, NULL,
              .new = 1, .offset = offsetof(struct priv, opts)},
+            {"outdir", NULL, &m_option_type_string,
+             .new = 1, .offset = offsetof(struct priv, outdir)},
             {0},
         },
     },
