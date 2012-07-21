c72cb2940a873b800114b43f58d5cf55cdccefc0
Stefano Pigozzi <stefano.pigozzi@gmail.com>
2012-07-21 12:07:37 +0200

    cocoa_common: don't require a depth buffer
    mplayer2's video outputs are not using an OpenGL depth buffer, so it's
    useless to specifically require for it.

--- libvo/cocoa_common.m
+++ libvo/cocoa_common.m
@@ -242,7 +242,6 @@ int vo_cocoa_create_window(struct vo *vo, uint32_t d_width,
 
         NSOpenGLPixelFormatAttribute attrs[] = {
             NSOpenGLPFADoubleBuffer, // double buffered
-            NSOpenGLPFADepthSize, (NSOpenGLPixelFormatAttribute)16, // 16 bit depth buffer
             (NSOpenGLPixelFormatAttribute)0
         };
 
